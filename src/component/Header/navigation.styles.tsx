a482eb30e452c604b2e2088056df60a6f9630c20
Zad Babaei <zadbabaei@gmail.com>
2022-02-16 17:37:59 -0330

    create the react-app and the navigation component

---
+++ src/component/Header/navigation.styles.tsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Navbar=styled.nav`
+    background-color: #34d1e6;
+    height: 30vh;
+    margin: 0;
+    padding:0;
+
+`
---
+++ src/component/Header/navigation.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import * as S from './navigation.styles'
+
+const navigation:React.FC=()=> {
+  return (
+      <S.Navbar>
+          <div>index</div>
+      </S.Navbar>
+  )
+}
+
+export default navigation
